ba71f82dac737bd40c0421f091d99ab79accb334
Parth Sharma <sharmaparth3930@gmail.com>
2024-11-30 10:12:33 +0530

    google verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><meta name="google-site-verification" content="v3Iw5zKTvl4nmqMkG_re_zltWnSWFgLey9wODp6ZwVU" /></head>
+      <head><meta name="google-site-verification" content="dz3x41Ls0YG91PBi3TfH7MciacoIrQKUkuWV6muAOMw" /></head>
       <body>{children}</body>
     </html>
   );
